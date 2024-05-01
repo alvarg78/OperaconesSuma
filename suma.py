9376b18d663f1c430bfd7255a21dc8af35757b10
Alvarg <magnificat1215@gmail.com>
2024-05-01 11:42:32 +0200

    Primer comentario

---
+++ suma.py
@@ -0,0 +1,11 @@
+
+
+def suma(a, b):
+    return a + b
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+resultado = suma(num1, num2)
+print("La suma de", num1, "y", num2, "es:", resultado)
+
